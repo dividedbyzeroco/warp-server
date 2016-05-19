0802f5a8baee5b0915688bbd01ffe7e9d739dd34
Jake Josol <jmjosol@hotmail.com>
2016-05-19 19:08:01 +0800

    Updated migrations

--- migration.js
+++ migration.js
@@ -29,7 +29,14 @@ _.extend(Migration.prototype, {
             'created_at': now.format('YYYY-MM-DD HH:mm:ss'),
             'updated_at': now.format('YYYY-MM-DD HH:mm:ss')
         })
-        .create();
+        .create()
+        .then(function() {
+            return {
+                id: this.id,
+                created_at: now.format('YYYY-MM-DD HH:mm:ss'),
+                updated_at: now.format('YYYY-MM-DD HH:mm:ss')
+            };
+        }.bind(this));
     },
     update: function() {
         // Update a migration
@@ -40,7 +47,13 @@ _.extend(Migration.prototype, {
             'down': JSON.stringify(this.down),
             'updated_at': now.format('YYYY-MM-DD HH:mm:ss')
         })
-        .update();
+        .update()
+        .then(function() {
+            return {
+                id: this.id,
+                updated_at: now.format('YYYY-MM-DD HH:mm:ss')
+            };
+        }.bind(this));
     },
     destroy: function() {
         // Update a migration
@@ -50,7 +63,14 @@ _.extend(Migration.prototype, {
             'updated_at': now.format('YYYY-MM-DD HH:mm:ss'),
             'deleted_at': now.format('YYYY-MM-DD HH:mm:ss')
         })
-        .update();
+        .update()
+        .then(function() {
+            return {
+                id: this.id,
+                updated_at: now.format('YYYY-MM-DD HH:mm:ss'),
+                deleted_at: now.format('YYYY-MM-DD HH:mm:ss')
+            };
+        }.bind(this));
     },
     commit: function() {
         // Execute Up
@@ -113,20 +133,168 @@ _.extend(Migration, {
     commit: function() {
         // Check if migrations are activated
         if(!Migration.activated) throw Migration._deactivatedError;
+        
         // Query all pending migrations
-        // Commit each
+        var query = new this._viewQuery(this.className);
+        var migrated = [];
+        
+        // Retrieve all migrations which have not been committed
+        // And which have not been deleted
+        // Then, sort the results in chronological order
+        return query.where({
+            'committed_at': {
+                'ex': false
+            },
+            'deleted_at': {
+                'ex': false
+            }
+        })
+        .sort([
+            { 'created_at': 1 }
+        ])
+        .find(function(migrations) {
+            // Check if pending migrations exist
+            if(migrations.length == 0) throw new WarpError(WarpError.Code.QueryError, 'No pending migrations found');
+            
+            // Create base promise
+            var promise = new Promise(function(resolve, reject) {
+                resolve();
+            });
+            
+            // Loop through each pending migration
+            for(var index in migrations)
+            {
+                // Retrieve migration
+                var def = migrations[index];
+                var migration = this.create(def.id, JSON.parse(def.up), JSON.parse(def.down));
+                
+                // Append a new promise to the base promise
+                promise = promise.then(function(previous) {
+                    if(previous) migrated.push(previous);
+                    return migration.commit();
+                });
+            }
+            
+            // Return the chained promises
+            // When completed, return the list of migrated items
+            return promise.then(function() {
+                return migrated;
+            });
+        }.bind(this))
+        .catch(function(error) {
+            console.error('[Warp Migration] Could not commit all migrations', error.message, error.stack);
+            throw new WarpError(WarpError.Code.InternalServerError, 'Could not commit all migrations: ' + error.message);
+        });
+    },
+    current: function() {
+        // Check if migrations are activated
+        if(!Migration.activated) throw Migration._deactivatedError;
+        
+        // Query the latest committed migration
+        var query = new this._viewQuery(this.className);
+        
+        // Retrieve the latest committed migration
+        // which has not been deleted
+        // Then, sort the results in reverse chronological order
+        return query.where({
+            'committed_at': {
+                'ex': true
+            },
+            'deleted_at': {
+                'ex': false
+            }
+        })
+        .sort([
+            { 'created_at': -1 }
+        ])
+        .first(function(def) {
+            // Check if commited migration exist
+            if(!def) throw new WarpError(WarpError.Code.QueryError, 'No migration has been committed yet');
+            
+            // Retrieve migration
+            var migration = this.create(def.id, JSON.parse(def.up), JSON.parse(def.down));
+                        
+            // Return the latest committed migration
+            return migration;
+        }.bind(this))
+        .catch(function() {            
+            console.error('[Warp Migration] Could not fetch the latest migration', error.message, error.stack);
+            throw new WarpError(WarpError.Code.InternalServerError, 'Could not fetch the latest migration: ' + error.message);
+        });
     },
     revert: function() {
         // Check if migrations are activated
         if(!Migration.activated) throw Migration._deactivatedError;
-        // Query latest committed migrations
-        // Revert       
+        
+        // Query the latest committed migration
+        var query = new this._viewQuery(this.className);
+        
+        // Retrieve the current migration
+        return this.current()
+        .then(function(migration) {                        
+            // Attempt to revert the migration
+            return migration.revert();
+        }.bind(this))
+        .catch(function(error) {
+            console.error('[Warp Migration] Could not revert latest migration', error.message, error.stack);
+            throw new WarpError(WarpError.Code.InternalServerError, 'Could not revert latest migration: ' + error.message);
+        });
     },
     reset: function() {
         // Check if migrations are activated
         if(!Migration.activated) throw Migration._deactivatedError;
-        // Query all committed migrations
-        // Revert each
+        
+        // Query the latest committed migrations
+        var query = new this._viewQuery(this.className);
+        var reverted = [];
+        
+        // Retrieve the latest committed migrations
+        // which have not been deleted
+        // Then, sort the results in reverse chronological order
+        return query.where({
+            'committed_at': {
+                'ex': true
+            },
+            'deleted_at': {
+                'ex': false
+            }
+        })
+        .sort([
+            { 'created_at': -1 }
+        ])
+        .find(function(migrations) {
+            // Check if commited migrations exist
+            if(migrations.length == 0) throw new WarpError(WarpError.Code.QueryError, 'No migration has been committed yet');
+            
+            // Create base promise
+            var promise = new Promise(function(resolve, reject) {
+                resolve();
+            });
+            
+            // Loop through each pending migration
+            for(var index in migrations)
+            {
+                // Retrieve migration
+                var def = migrations[index];
+                var migration = this.create(def.id, JSON.parse(def.up), JSON.parse(def.down));
+                
+                // Append a new promise to the base promise
+                promise = promise.then(function(previous) {
+                    if(previous) reverted.push(previous);
+                    return migration.revert();
+                });
+            }
+            
+            // Return the chained promises
+            // When completed, return the list of migrated items
+            return promise.then(function() {
+                return reverted;
+            });
+        }.bind(this))
+        .catch(function() {            
+            console.error('[Warp Migration] Could not reset migrations', error.message, error.stack);
+            throw new WarpError(WarpError.Code.InternalServerError, 'Could not reset migrations: ' + error.message);
+        });
     }
 });
 
