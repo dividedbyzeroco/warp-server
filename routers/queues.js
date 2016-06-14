5d33d8d255d28c2388d6aa5bbdd4423f0f62afd7
Jake Josol <jmjosol@hotmail.com>
2016-06-14 11:51:04 +0800

    Improved error handling

--- routers/queues.js
+++ routers/queues.js
@@ -3,31 +3,56 @@ var middleware = require('./middleware');
 
 module.exports = {
     status: function(req, res, next) {    
-        var name = req.params.name;
-        var isActive = this._getQueue(name).isActive();
-        var latestSuccess = this._getQueue(name).getLatestSuccess();
-        var latestError = this._getQueue(name).getLatestError();
-        var result = { 
-            name: name, 
-            is_active: isActive, 
-            latest_success: latestSuccess, 
-            latest_error: latestError 
-        };
-        res.json({ status: 200, message: 'Success', result: result });
+        try
+        {
+            var name = req.params.name;
+            var isActive = this._getQueue(name).isActive();
+            var latestSuccess = this._getQueue(name).getLatestSuccess();
+            var latestError = this._getQueue(name).getLatestError();
+            var result = { 
+                name: name, 
+                is_active: isActive, 
+                latest_success: latestSuccess, 
+                latest_error: latestError 
+            };
+            res.json({ status: 200, message: 'Success', result: result });
+        }
+        catch(ex)
+        {
+            next(ex);
+        }
     },
-    start: function(req, res, next) {    
-        var name = req.params.name;
-        res.json({ status: 200, message: 'Success', result: { name: name } });
-        
-        // Run action
-        this._getQueue(name).start();
+    start: function(req, res, next) {
+        try
+        {    
+            var name = req.params.name;
+                        
+            // Run action
+            this._getQueue(name).start();
+            
+            // Return response
+            res.json({ status: 200, message: 'Success', result: { name: name } });
+        }
+        catch(ex)
+        {
+            next(ex);
+        }
     },
     stop: function(req, res, next) {
-        var name = req.params.name;
-        res.json({ status: 200, message: 'Success', result: { name: name } });
-        
-        // Run action
-        this._getQueue(name).stop();
+        try
+        {
+            var name = req.params.name;
+            
+            // Run action
+            this._getQueue(name).stop();
+            
+            // Return response
+            res.json({ status: 200, message: 'Success', result: { name: name } });
+        }
+        catch(ex)
+        {
+            next(ex);
+        }
     },
     apply: function(context, router) {
         var masterKeyRequired = middleware.requireMasterKey(context._config.security.masterKey);
