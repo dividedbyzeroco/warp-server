082307041f6de0c30987c45d06204dec34318f61
Jason Josol <josol@mclinica.com>
2018-03-23 15:40:00 +0800

    Made find options optional

--- src/types/sessions.ts
+++ src/types/sessions.ts
@@ -1,14 +1,14 @@
 export type FindOptionsType = {
-    select: Array<string>,
-    include: Array<string>,
-    where: {[name: string]: {[name: string]: any}},
-    sort: Array<string | {[name: string]: any}>,
-    skip: number,
-    limit: number
+    select?: Array<string>,
+    include?: Array<string>,
+    where?: {[name: string]: {[name: string]: any}},
+    sort?: Array<string | {[name: string]: any}>,
+    skip?: number,
+    limit?: number
 }
 
 export type GetOptionsType = {
     id: number,
-    select: Array<string>,
-    include: Array<string>
+    select?: Array<string>,
+    include?: Array<string>
 };
